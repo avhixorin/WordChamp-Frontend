6b7ee856f2a36aea1b6d4f4c799fa32653618a69
avhixorin <avhixorin@gmail.com>
2024-11-13 01:28:51 +0530

    feat: remove overflow-auto class from LeaderBoard component for improved scrolling behavior

--- src/components/Game/LeaderBoard/LeaderBoard.tsx
+++ src/components/Game/LeaderBoard/LeaderBoard.tsx
@@ -40,7 +40,7 @@ const LeaderBoard: React.FC = () => {
   return (
     <div className="bg-transparent text-zinc-700 p-6 rounded-lg text-center backdrop-blur-xl w-full font-super">
       <h2 className="text-3xl font-bold mb-4">LeaderBoard</h2>
-      <div className="overflow-auto max-h-[300px]">
+      <div className="max-h-[300px]">
         {gameMode === GameMode.MULTIPLAYER ? (
           <table className="w-full">
             <thead>
